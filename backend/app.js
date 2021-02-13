9a19745e794f503cfc4e788e626e77d200c9e6ab
geotrieu <trieugeorge@gmail.com>
2021-02-13 18:54:46 -0500

    routes working

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
-var express = require("express"),
-    app = express(),
-    port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const config = require("config");
+const express = require("express");
+
+app = express();
+port = process.env.PORT || 3000;
+
+require("./routes/routes")(app);
+
+mongoose.connect(
+    config.get("database.connection") + config.get("database.database"),
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 
 app.listen(port);
 
--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,43 +1,36 @@
-'use strict';
+const mongoose = require("mongoose");
+Task = mongoose.model("Tasks", {});
 
-
-var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
-
-exports.list_all_courses = function(req, res) {
-  Task.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+exports.list_all_courses = function (req, res) {
+    Task.find({}, function (err, task) {
+        if (err) res.send(err);
+        res.json(task);
+    });
 };
 
-
-exports.add_a_course = function(req, res) {
-  var new_task = new Task(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+exports.add_a_course = function (req, res) {
+    var new_task = new Task(req.body);
+    new_task.save(function (err, task) {
+        if (err) res.send(err);
+        res.json(task);
+    });
 };
 
-
-exports.get_course = function(req, res) {
-  Task.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
+exports.get_course = function (req, res) {
+    Task.findById(req.params.taskId, function (err, task) {
+        if (err) res.send(err);
+        res.json(task);
+    });
 };
 
-
-exports.delete_course = function(req, res) {
-  Task.remove({
-    _id: req.params.taskId
-  }, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Course successfully deleted' });
-  });
+exports.delete_course = function (req, res) {
+    Task.remove(
+        {
+            _id: req.params.taskId,
+        },
+        function (err, task) {
+            if (err) res.send(err);
+            res.json({ message: "Course successfully deleted" });
+        }
+    );
 };
