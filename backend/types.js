709e1be3d204ff60ff01b2ecc1ccfbaf7473902b
Aree-Vanier <git@gregk.ca>
2021-02-13 15:25:31 -0500

    Added types for Assignment and course

---
+++ backend/types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef  {object} Assignment
+ * @property {string} name
+ * @property {numner} timestamp
+ * @property {number} weight
+ * 
+ * @typedef  {object} Course
+ * @property {number} id
+ * @property {string} code
+ * @property {string} name
+ * @property {string} author
+ * @property {number} subscriptions
+ * @property {Assignment[]} assignments
+ */
