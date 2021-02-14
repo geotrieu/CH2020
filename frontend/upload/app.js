7bfcdc29a36b4104835aedd474490b747f39e08a
geotrieu <trieugeorge@gmail.com>
2021-02-13 23:37:49 -0500

    Added row fill

--- frontend/upload/app.js
+++ frontend/upload/app.js
@@ -21,11 +21,25 @@ window.onload = function () {
     )[0].onclick = async function () {
         const file = document.getElementById("fileInputButton").files[0];
         if (file != null) {
-            console.log(file);
-            console.log(await postPDF(file));
+            const assessments = await postPDF(file);
+            assessments.forEach((a) => {
+                addNewRow(a.item, formatDate(a.date), a.weight);
+            });
         }
     };
 };
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = "" + (d.getMonth() + 1),
+        day = "" + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [year, month, day].join("-");
+}
 
 async function postPDF(file) {
     // Default options are marked with *
@@ -38,18 +52,18 @@ async function postPDF(file) {
     return response.json(); // parses JSON response into native JavaScript objects
 }
 
-function addNewRow() {
+function addNewRow(name, deadline, weight = 1) {
     let newEl = document.createElement("TR");
     newEl.classList.add("tableRow");
     newEl.innerHTML = `
     <td class="tableCell name">
-      <input class="tableInput" type="text" />
+      <input class="tableInput" type="text" value="${name}" />
     </td>
     <td class="tableCell deadline">
-      <input class="tableInput" type="date" />
+      <input class="tableInput" type="date" value="${deadline}" />
     </td>
     <td class="tableCell weight">
-      <input class="tableInput percent" type="number" min="1" max="100" value="1" />
+      <input class="tableInput percent" type="number" min="1" max="100" value="${weight}" />
     </td>
     <td><img src="../assets/trash.svg" class="delete" /></td>
   `;
