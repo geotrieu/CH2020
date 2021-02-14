b03148b828e1dd2d92deb18bbc77ee1491784b40
Aree-Vanier <git@gregk.ca>
2021-02-13 21:10:11 -0500

    Parsing results sent to frontend

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -5,16 +5,14 @@ exports.handleSyllabus = function(req, res) {
     let file = req.files[0];
     if(file.mimetype === "application/pdf"){
         console.log(file);
-        fs.rename(file.destination +"/" + file.filename, file.destination + "/" + file.filename+".pdf", ()=>{
-            parsePDF(file.destination + "/" + file.filename+".pdf", req.query.code);
+        fs.rename(file.destination +"/" + file.filename, file.destination + "/" + file.filename+".pdf", async ()=>{
+            res.json(await parsePDF(file.destination + "/" + file.filename+".pdf", req.query.code));
         });
-        res.send("Complete");
     } else {
         res.send("Invalid file type");
     }
 }
 
 async function parsePDF(path, courseCode){
-    let assesments = await getAssesments(path, courseCode);
-    console.log(assesments);
+    return await getAssesments(path, courseCode);
 }
